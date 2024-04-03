21e521d1ce04e91dc44c221af36e986b07374dc7
azeezabidoye <azeezabidoye@yahoo.com>
2024-04-03 12:04:51 +0100

    Search query added to component

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -2,10 +2,14 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: 1234567 },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [searchQuery, setSearchQuery] = useState("ade");
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -34,9 +38,38 @@ const App = () => {
     setNewNumber(event.target.value);
   };
 
+  const handleSearchName = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
+  // Function to search for names in the saerch bar
+  const filteredNames = persons.filter((person) => {
+    // person.name.toLowerCase().includes(searchQuery.toLowerCase());
+    if (person.name.toLowerCase() === searchQuery.toLowerCase()) {
+      return person.name;
+    }
+  });
+
+  console.log(filteredNames);
+  // const filterNames = (namesArr) => {
+  //   return namesArr.name.toLowerCase().includes(searchQuery.toLowerCase());
+  // };
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <form>
+        <div>
+          Find name: <input value={searchQuery} onChange={handleSearchName} />
+        </div>
+      </form>
+
+      <ul>
+        {filteredNames.map((name, index) => {
+          return <li key={index}>{name}</li>;
+        })}
+      </ul>
+
       <form onSubmit={addPerson}>
         <div>
           Name: <input value={newName} onChange={handleNameChange} />
@@ -48,11 +81,11 @@ const App = () => {
           <button type="submit">add</button>
         </div>
       </form>
+
       <h2>Numbers</h2>
-      {/* <p>{persons[1].name}</p> */}
       <ul>
         {persons.map((person) => (
-          <li key={person.name}>
+          <li key={person.id}>
             {person.name}: {person.number}
           </li>
         ))}
